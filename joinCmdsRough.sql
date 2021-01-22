e57c845dced6e64f55324ff0b38ba663e9667420
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-01-21 16:23:08 -0800

    cleaned up joinCmdsRough.sql code

--- joinCmdsRough.sql
+++ joinCmdsRough.sql
@@ -15,8 +15,3 @@ FROM role INNER JOIN department ON (role.department_id=department.id) INNER JOIN
 LEFT JOIN employee AS b ON (a.manager_id=b.id);
 
 -- FROM employee AS a left
-
-
-
--- CONCAT(col 1, ' ', col 2) AS manager
--- dunno how to reference employee's manager! Maybe join to itself?
